2fefbeb49fedd4eee0a0552021599b484e2b2b62
Sam-Shorty <sam.shorty00@gmail.com>
2022-03-31 11:06:11 +0200

    script and conf to generate clients

---
+++ base-server/make-client.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# First argument: Client name
+
+# Argoument check
+if [ $# -eq 0 ]
+then 
+    echo 'Invalid number of argoument'
+    exit 
+fi
+
+# Setup variable
+cd ..
+SERVER_DIR=${PWD}
+CLIENT_DIR=${PWD}/client-configs/${1}
+BASE_CONFIG=${PWD}/client-configs/base.conf
+
+echo -e "Using the following variable:"
+echo -e "SERVER_DIR: ${SERVER_DIR}"
+echo -e "CLIENT_DIR: ${CLIENT_DIR}"
+echo -e "BASE_CONFIG: ${BASE_CONFIG}"
+
+echo -e "Create client folder...\n"
+mkdir ${CLIENT_DIR}
+
+echo -e "Create client key and crt...\n"
+cd ${SERVER_DIR}/easy-rsa
+./easyrsa gen-req ${1} nopass
+./easyrsa sign-req client ${1}
+
+echo -e "Copy various file...\n"
+cd ${SERVER_DIR}
+cp easy-rsa/pki/issued/${1}.crt client-configs/${1}
+cp easy-rsa/pki/private/${1}.key client-configs/${1}
+cp ta.key client-configs/${1}
+cp ca.crt client-configs/${1}
+
+echo -e "Generating OVPN...\n"
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${CLIENT_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${CLIENT_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${CLIENT_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-crypt>') \
+    ${CLIENT_DIR}/ta.key \
+    <(echo -e '</tls-crypt>') \
+    > ${CLIENT_DIR}/${1}.ovpn
