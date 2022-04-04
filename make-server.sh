2de7851325378fdcf94a427c9319e4fee342a6bb
Sam-Shorty <sam.shorty00@gmail.com>
2022-04-04 15:26:51 +0200

    update and improved legibility

--- make-server.sh
+++ make-server.sh
@@ -8,7 +8,16 @@
 # Argoument check
 if [ $# -eq 0 ]
 then 
-    echo "Invalid number of argoument"
+    echo "make_openvpn script by sam.shorty"
+    echo -e "\n"
+    echo "Please use the following arguments:"
+    echo "1 -> Name of the VPN"
+    echo "2 -> IP of the VPN"
+    echo "3 -> Port of the VPN"
+    echo "4 -> IP of the pool address for the VPN"
+    echo "Example: ./make_openvpn test myip.com 1194 10.8.0.0"
+    echo -e "\n"
+    echo "Run this script directly in the folder where it is installed and where you want the VPN to be created"
     exit 
 fi
 
@@ -16,12 +25,12 @@ fi
 SERVER_DIR=${PWD}/${1}
 WORKING_DIRECTORY=${PWD}
 
-echo -e "Using the following variable:\n"
+echo -e "Using the following variable:"
 echo "SERVER_DIR: ${SERVER_DIR}"
 echo "IP: ${2}"
 echo "PORT: ${3}"
 
-echo -e "\nCreating server directory...\n"
+echo -e "\nCreating server directory..."
 mkdir ${SERVER_DIR}
 
 echo -e "Creating easy-rsa infrastructure...\n"
@@ -42,21 +51,24 @@ echo -e "Importing and sign server req...\n"
 ./easyrsa sign-req server server
 
 echo -e "Generating ta.key...\n"
+cd ..
 openvpn --genkey secret ta.key
 
-echo -e "Copy various file...\n"
-cp pki/private/server.key ${SERVER_DIR}
-cp pki/issued/server.crt ${SERVER_DIR}
-cp pki/ca.crt ${SERVER_DIR}
-cp ta.key ${SERVER_DIR}
+echo -e "Copy various file..."
+echo -e "Copy server.key from ${SERVER_DIR}/easy-rsa/pki/private/server.key to ${SERVER_DIR}"
+cp easy-rsa/pki/private/server.key ${SERVER_DIR}
+echo -e "Copy server.crt from ${SERVER_DIR}/easy-rsa/pki/issued/server.crt to ${SERVER_DIR}"
+cp easy-rsa/pki/issued/server.crt ${SERVER_DIR}
+echo -e "Copy ca.crt from ${SERVER_DIR}/easy-rsa/pki/ca.crt to ${SERVER_DIR}"
+cp easy-rsa/pki/ca.crt ${SERVER_DIR}
 
-echo -e "Creating server.conf...\n"
+echo -e "Creating server.conf..."
 cd ${SERVER_DIR}
 cp ${WORKING_DIRECTORY}/base-server/server.conf server.conf
 sed -i "s:{PORT}:${3}:g" server.conf
 sed -i "s:{IP_POOL}:${4}:g" server.conf
 
-echo -e "Creating clients...\n"
+echo -e "Creating clients..."
 mkdir clients
 cp ${WORKING_DIRECTORY}/base-server/make-client.sh clients
 chmod 700 clients/make-client.sh
@@ -64,11 +76,12 @@ cp ${WORKING_DIRECTORY}/base-server/base.conf clients
 sed -i "s:{IP}:${2}:g" clients/base.conf
 sed -i "s:{PORT}:${3}:g" clients/base.conf
 
-echo -e "Creating ccd adn log dir...\n"
+echo -e "Creating ccd dir"
 mkdir ccd
+echo -e "Creating log dir"
 mkdir log
 
-echo -e "Creating service...\n"
+echo -e "Creating service..."
 cd ${WORKING_DIRECTORY}
 cp base-server/openvpn-base.service base-server/openvpn-${1}.service
 sed -i "s:{NAME}:${1}:g" base-server/openvpn-${1}.service
